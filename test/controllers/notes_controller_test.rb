240ba2f8a3c74f1c0c612be6db9ccd1a585c745f
Caleb775 <89751339+Caleb775@users.noreply.github.com>
2025-10-02 08:54:11 +0900

    git test trails

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -2,22 +2,28 @@ require "test_helper"
 
 class NotesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get notes_index_url
+    student = students(:one)
+    get student_notes_url(student)
     assert_response :success
   end
 
   test "should get show" do
-    get notes_show_url
+    student = students(:one)
+    note = notes(:one)
+    get student_note_url(student, note)
     assert_response :success
   end
 
   test "should get new" do
-    get notes_new_url
+    student = students(:one)
+    get new_student_note_url(student)
     assert_response :success
   end
 
   test "should get edit" do
-    get notes_edit_url
+    student = students(:one)
+    note = notes(:one)
+    get edit_student_note_url(student, note)
     assert_response :success
   end
 end
--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -2,22 +2,24 @@ require "test_helper"
 
 class StudentsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get students_index_url
+    get students_url
     assert_response :success
   end
 
   test "should get show" do
-    get students_show_url
+    student = students(:one)
+    get student_url(student)
     assert_response :success
   end
 
   test "should get new" do
-    get students_new_url
+    get new_student_url
     assert_response :success
   end
 
   test "should get edit" do
-    get students_edit_url
+    student = students(:one)
+    get edit_student_url(student)
     assert_response :success
   end
 end
